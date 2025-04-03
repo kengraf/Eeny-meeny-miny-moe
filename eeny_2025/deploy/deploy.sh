49186c246c4c00f890990ce26bae833c597a47b7
Ken Graf <ken.graf@cs.unh.edu>
2025-04-03 17:10:35 -0400

    Update deploy.sh

--- eeny_2025/deploy/deploy.sh
+++ eeny_2025/deploy/deploy.sh
@@ -26,4 +26,4 @@ STACK_ID=`aws cloudformation deploy --stack-name ${STACK} \
   --parameter-overrides  S3BUCKET=$S3BUCKET EMAIL=${EMAIL} \
   --region ${REGION} --query "StackId" --output text`
 
-aws cloudformation list-exports --query "Exports[?Name=='FetchFunctionUrl'].Value" --output text
+aws cloudformation list-exports --query "Exports[?Name=='eeny2025-FetchFunctionUrl'].Value" --output text
