0ca5b46e6d537e8be5e00d8277cd4de06d442404
Ken Graf <ken.graf@cs.unh.edu>
2025-04-03 12:34:36 -0400

    Update deploy2025.sh

--- eeny_2025/deploy/deploy2025.sh
+++ eeny_2025/deploy/deploy2025.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-STACK=Eeny2025
-S3BUCKET=eeny5-2025
-REGION=us-west-2
+# Edit the following parameters to suit your needs.  
+# The S3 bucket needs to be globally unique
+STACK=eeny2025
+S3BUCKET=eeny-2025
+REGION=us-east-2
 
 if aws s3api head-bucket --bucket "$S3BUCKET" --region ${REGION} 2>/dev/null; then
   echo "Bucket '$S3BUCKET' exists."
@@ -11,16 +13,15 @@ else
   aws s3api create-bucket --bucket ${S3BUCKET} --region ${REGION}
 fi
     
-echo "Load lambdas to S3 bucket..."
-pushd lambda/fetch2025
-zip fetch.zip -xi index.py
-aws s3 cp fetch2025.zip s3://${S3BUCKET}
-popd
+echo "Load lambda and friends to S3 bucket..."
+zip eeny.zip -xi eeny.py
+aws s3 cp eeny.zip s3://${S3BUCKET}
+aws s3 cp friends s3://${S#BUCKET}
 
 echo "Creating stack..."
 # upload cf stack
 STACK_ID=`aws cloudformation deploy --stack-name ${STACK} \
-  --template-body file://cfStack2025.json --capabilities CAPABILITY_NAMED_IAM \
+  --template-body file://cfStack2025.yaml --capabilities CAPABILITY_NAMED_IAM \
   --tags Key=DeployName,Value=${STACK} \
   --region ${REGION} --query "StackId" --output text`
 
